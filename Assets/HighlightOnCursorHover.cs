f9a238899941e8c7e8d7d7ea40e42f7cf2fd0ad6
Dan Andrus <danieleandrus@gmail.com>
2021-04-25 12:27:28 -0700

    Move raycasting to reusable location

--- Assets/HighlightOnCursorHover.cs
+++ Assets/HighlightOnCursorHover.cs
@@ -4,36 +4,23 @@ using UnityEngine;
 
 public class HighlightOnCursorHover : MonoBehaviour
 {
-    public GameObject rayEmitter;
+    public GameObject baseSelectionOnOrientationOf;
 
     private Material myMaterial;
     private Color originalColor;
+    private IsSelectedComputer isSelectedComputer;
 
     // Start is called before the first frame update
     void Start()
     {
         myMaterial = GetComponent<MeshRenderer>().material;
         originalColor = myMaterial.color;
+        isSelectedComputer = new IsSelectedComputer(gameObject, baseSelectionOnOrientationOf);
     }
 
     // Update is called once per frame
     void Update()
     {
-        myMaterial.color = isHighlighted() ? Color.green : originalColor;
-    }
-
-    private bool isHighlighted() {
-        Ray ray = new Ray(rayEmitter.transform.position, rayEmitter.transform.rotation * Vector3.forward);
-
-        Debug.DrawRay(ray.origin, ray.direction, Color.red, 100);
-
-        RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction, 100);
-
-        foreach (RaycastHit hit in hits) {
-          if (hit.collider.gameObject == gameObject) {
-              return true;
-          }
-        }
-        return false;
+        myMaterial.color = isSelectedComputer.isHighlighted() ? Color.green : originalColor;
     }
 }
